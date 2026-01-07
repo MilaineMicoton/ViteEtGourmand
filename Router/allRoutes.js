cae25bc09b31a579bab5cf791ebf513b1f52ef69
Milaine <marieheleneknight46@gmail.com>
2026-01-07 21:52:18 +0100

    Ajout système routage terminé

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,9 +2,7 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-
-];
+    new Route("/", "Accueil", "/pages/home.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
-export const websiteName = "Vite et Gourmand";
+export const websiteName = "Vite et gourmand";
